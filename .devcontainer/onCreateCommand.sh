7c80e7a6e68ec4a97a0a338508dbed3db5b7da3c
Craig Peters <craiglpeters@github.com>
2023-04-26 17:31:29 +0000

    Moving pytorch to onCreateCommand

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
 pip install --upgrade pip
 pip install wheel
 pip install openvino-dev==2022.1.0 # [OPTIONAL] to generate optimized models for inference
 pip install mlcube_docker # [OPTIONAL] to deploy GaNDLF models as MLCube-compliant Docker containers
+pip install torch==1.13.1+cpu torchvision==0.14.1+cpu torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cpu
